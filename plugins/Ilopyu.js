e905bde040f6f59c2b7e85af63219a4e8873c9e0
RezaOfc10 <113519101+RezaOfc10@users.noreply.github.com>
2022-09-18 16:11:34 +0700

    Create Ilopyu.js

---
+++ plugins/Ilopyu.js
@@ -0,0 +1,16 @@
+let fs = require('fs')
+let handler = async (m) => {
+let helloaine = fs.readFileSync('mp3/PTT-20211218-WA0243.opus')
+conn.sendFile(m.chat, helloaine, '', '', m, true)
+}
+
+handler.customPrefix = /^(ilopyu|iloveyou|i love you|lopyu|ilovyu|loveyou|loveyou|love|❤)$/i
+handler.command = new RegExp
+
+handler.limit = true
+handler.mods = false 
+handler.premium = false
+handler.group = false 
+handler.private = false
+
+module.exports = handler
